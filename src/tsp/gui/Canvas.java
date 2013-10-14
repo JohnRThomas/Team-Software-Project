a339bbe43a376892abf960a6f93607b34112d6ef
John Thomas <jrthomas@mtu.edu>
2013-10-14 14:58:42 -0400

    Touch works with a small delay

--- src/tsp/gui/Canvas.java
+++ src/tsp/gui/Canvas.java
@@ -15,59 +15,37 @@ public class Canvas extends JPanel implements KeyListener{
 		super();
 		this.addKeyListener(this);
 	}
-	boolean up = true;
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
-		up = false;
-		while(!up){
-			switch(e.getKeyCode()){
-			case KeyEvent.VK_LEFT:
-				if(player.x > 10)player.x -= 10;
-				else player.x = 10;
-				break;
-			case KeyEvent.VK_RIGHT:
-				if(player.x <= getBounds().width)player.x += 10;
-				else player.x = getBounds().width -20;
-				break;
-			case KeyEvent.VK_DOWN:
-				if(player.y <= this.getBounds().height)player.y += 10;
-				else player.y = getBounds().height - 20;
-				break;
-			case KeyEvent.VK_UP:
-				if(player.y > 10)player.y -= 10;
-				else player.y = 10;
-				break;	
-			}
-			repaint();
-		}
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		/*switch(e.getKeyCode()){
-		case KeyEvent.VK_LEFT:
+		switch(e.getKeyChar()){
+		case 'a':
 			if(player.x > 10)player.x -= 10;
 			else player.x = 10;
 			break;
-		case KeyEvent.VK_RIGHT:
+		case 'd':
 			if(player.x <= getBounds().width)player.x += 10;
 			else player.x = getBounds().width -20;
 			break;
-		case KeyEvent.VK_DOWN:
+		case 's':
 			if(player.y <= this.getBounds().height)player.y += 10;
 			else player.y = getBounds().height - 20;
 			break;
-		case KeyEvent.VK_UP:
+		case 'w':
 			if(player.y > 10)player.y -= 10;
 			else player.y = 10;
 			break;	
 		}
-		repaint();*/
+		repaint();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		up = true;
 	}
 
 	@Override
