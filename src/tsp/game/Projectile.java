3f25ee059d514304e7aac6db54835953906b36aa
Kevin Schmitt <kjschmit@mtu.edu>
2013-11-04 15:04:38 -0500

    Projectile Class

---
+++ src/tsp/game/Projectile.java
@@ -0,0 +1,18 @@
+package tsp.game;
+
+import imageBase.BaseImage;
+
+public class Projectile extends BaseImage {
+
+	public Projectile(int xStart, int yStart, String imageName, boolean collide) {
+		super(xStart, yStart, imageName, collide);
+		
+	}
+
+	@Override
+	public void collideWith(BaseImage entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
